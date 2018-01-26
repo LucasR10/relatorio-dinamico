9db623a1de5b5edcd065b6f7137811a24e564499
Lucas <lucas.carvalho@basis.com.br>
2018-01-26 14:42:52 -0200

    .....

--- src/main/java/br/com/relatoriodinamico/Config.java
+++ src/main/java/br/com/relatoriodinamico/Config.java
@@ -1,5 +1,28 @@
 package br.com.relatoriodinamico;
+import static net.sf.dynamicreports.report.builder.DynamicReports.*;
 
 public class Config {
+	
+	   
+	    private void build() {
+	      try {
+	        report()//create new report design
+	          .columns() //adds columns
+	          .groupBy() //adds groups
+	          .subtotalsAtSummary() //adds subtotals
+	          //set datasource
+	          .setDataSource()
+	          //export report
+	          .toPdf(true) //export report to pdf
+	          .toJasperPrint() //creates jasperprint object
+	          .show() //shows report
+	          .print(); //prints report
+	       
+	      } catch (DRException e) {
+	        e.printStackTrace(); 
+	      }
+	    } 
+	  }
+
 
 }
